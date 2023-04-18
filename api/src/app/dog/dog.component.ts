9ac42a931b541ae93e420266c067244141b01525
João Gabriel Z.B <71906858+JoaoGabrielBertolucci@users.noreply.github.com>
2023-04-18 11:48:44 -0300

    edição de arquivos

--- api/src/app/dog/dog.component.ts
+++ api/src/app/dog/dog.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { Dog } from '../dog';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Dog } from './dog';
+import { Injectable } from '@angular/core';
 
 @Component({
   selector: 'app-dog',
@@ -15,5 +19,33 @@ export class DogComponent {
   getName(){
     return this.dog.message.split("/")[4].split("-");
   }
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.loadDog();
+  }
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.LoadDog();
+  }
+
+  funtionLoadDog(){
+    
+  }
+  getRandom(): Observable<Dog>{
+      return this.http.get<Dog>("https://dog.ceo/api/breeds/image/random");
+  }
+
+
+   LoadDog(){
+    this.Dogservice.
+  }
+  getRandom(): Observable<Dog>{
+      return this.http.get<Dog>("https://dog.ceo/api/breeds/image/random");
+  }
+
 
 }
